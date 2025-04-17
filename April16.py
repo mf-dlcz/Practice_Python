25087dc7a3e9592429409ce737246cc0123b3ed8
Maria Delacruz <mar23iaa@gmail.com>
2025-04-16 22:36:44 -0700

    April 16 exercises

---
+++ April16.py
@@ -0,0 +1,18 @@
+#problem 1
+#for i in range(1,101):
+    #print(i)
+
+#problem 2
+hobby = "Hiking"
+#for each_letter in hobby:
+    #print(each_letter, end=" ")
+
+#problem 3
+#for i in range(0, len(hobby)):
+#    print(i,hobby[i])
+
+#problem 4
+#Counts down from 5 to 1 and then prints "Blast off!"
+for i in range(5,0,-1):
+    print(i)
+print("Blast off!")
