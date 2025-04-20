78c943f2be3236951f0b474a5319060bf34ee5fe
Maria Delacruz <mar23iaa@gmail.com>
2025-04-20 01:20:29 -0700

    Added more exercises

---
+++ April19.2.py
@@ -0,0 +1,33 @@
+# Exercise 1
+'''
+a = 40
+b = 11
+
+print(a == b)  #False
+print(a != b)  #True
+print(a > b)  #True
+print(a < b)  #False
+print(a >= b)  #True
+print(a <= b)  # False
+'''
+
+# Exercise 2
+'''
+m = 10
+d = 4
+s = 11
+
+print(m == d) #False
+print(m != d) #True
+print(m > d) #True
+print(m < d) #False
+print(m >= d) #True
+print(m <= d) #False
+
+print(m > s and s > d) #False
+print(s > m or s < d) #True
+'''
+
+# Exercise 3
+print("apple" == "orange") #False
+print("apple" != "orange") #True
