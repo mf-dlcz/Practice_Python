829dcab5f64d3a5c1227268b673ade9654e92227
Maria Delacruz <mar23iaa@gmail.com>
2025-04-21 16:20:41 -0700

    Added new exercises

---
+++ class/April21.py
@@ -0,0 +1,22 @@
+#Exercise 1: Guessing Game
+
+secret_number = 20
+guess = input("Enter your guess: ")
+#Can I validate the input is a number before I do my comparison?
+
+while guess != secret_number:
+  
+  #Converts the input to an integer
+  guess = int(guess)
+  if guess == secret_number:
+      print("You guessed it right!")
+  elif 
+
+
+
+#Exercise 2:
+'''
+Get user input for numbers continuosly until 'quit'
+Compute the average of these numbers.
+Find the manimum and minimum 
+'''
---
+++ myPracticeExercises/April21_2.py
@@ -0,0 +1,3 @@
+#If you mix a number with a float. The result will be a float.
+#Example 1:
+print(30 + 4.8)   #Output: 34.8
