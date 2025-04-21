b1de3a69839b16ee1d845576379e233a8fe290de
Maria Delacruz <mar23iaa@gmail.com>
2025-04-21 16:31:58 -0700

    Worked on an exercise

--- myPracticeExercises/April21_2.py
+++ myPracticeExercises/April21_2.py
@@ -1,3 +1,22 @@
 #If you mix a number with a float. The result will be a float.
-#Example 1:
-print(30 + 4.8)   #Output: 34.8
+# Example 1:
+#print(30 + 4.8)   #Output: 34.8
+
+#Exercise 2:
+# Define two variables: one integer and one float
+a = 4  # Replace with any integer value
+b = 10.11  # Replace with any float value
+# Perform the operations and print the results
+# Add your predictions in the comments before running the code
+# Example:
+# Addition
+# Predict: float
+print(a + b) #Output: 14.11
+# Now complete subtraction, multiplication, and division below!
+
+# Multiplication
+print(a * b) #Output: 40.44
+# Subtraction
+print(a - b) #Output: -6.109
+# division
+print(a / b) #Output: 0.39
