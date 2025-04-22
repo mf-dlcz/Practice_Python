d5283a4a6150d95dae2e0ce15bc3c5c1180dd5ba
Maria Delacruz <mar23iaa@gmail.com>
2025-04-21 17:11:34 -0700

    Worked on more practice problems

--- myPracticeExercises/April21_2.py
+++ myPracticeExercises/April21_2.py
@@ -1,22 +1,65 @@
-#If you mix a number with a float. The result will be a float.
-# Example 1:
+#If you mix a number with a float. The result will be always be a float.
+# Exercise 1:
 #print(30 + 4.8)   #Output: 34.8
 
-#Exercise 2:
+
+# Exercise 2:
 # Define two variables: one integer and one float
-a = 4  # Replace with any integer value
-b = 10.11  # Replace with any float value
+#a = 4  # Replace with any integer value
+#b = 10.11  # Replace with any float value
 # Perform the operations and print the results
 # Add your predictions in the comments before running the code
 # Example:
 # Addition
 # Predict: float
-print(a + b) #Output: 14.11
+#print(a + b) #Output: 14.11
 # Now complete subtraction, multiplication, and division below!
-
 # Multiplication
-print(a * b) #Output: 40.44
+#print(a * b) #Output: 40.44
 # Subtraction
-print(a - b) #Output: -6.109
+#print(a - b) #Output: -6.109
 # division
-print(a / b) #Output: 0.39
+#print(a / b) #Output: 0.39
+
+
+# Exercise 3:
+#m = 33
+#d = 11.5
+#print( type(m) ) #Output: int
+#print( type(d) ) #Output: float
+
+
+# Exercise 4:
+#m = "Maria"
+#b = False
+#print( type(m) ) #Output: str
+#print( type(b) ) #Output: boolean / bool
+
+
+# Exercise 5:
+#Casting
+#x = float(3)
+#y = str(2)
+#z = int(2.8)
+
+#print(x) #Output 3.0
+#print(y) #Output "2"
+#print(z) #Output 2
+
+
+# Exercise 6:
+#Indexing
+#name = "María"
+favoriteCity = "New York"
+#print(name[2]) #Output: "r"
+#print(name[0]) #Output: "M"
+#print(name[3]) #Output: "í"
+  #Slicing
+print(favoriteCity[0:3]) #Output: "New"
+print(favoriteCity[4:8]) #Output: "York"
+# Leave the begining empty to slice from begining to index 3
+print(favoriteCity[:3]) #Output: "New"
+# Leave the end empty to slice from desired index to the end
+print(favoriteCity[4:]) #Output: "York"
+
+#Exercise 7:
