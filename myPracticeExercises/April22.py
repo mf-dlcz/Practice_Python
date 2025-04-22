4e5ee9ae7fc3ae02b2e450c9fb50205acf4634c9
Maria Delacruz <mar23iaa@gmail.com>
2025-04-22 08:56:10 -0700

    Learned more about for loops and began learning about while loops

---
+++ myPracticeExercises/April22.py
@@ -0,0 +1,33 @@
+# Use rang() to loop & access the elements of an iterable using an index
+
+'''             Example:
+for <counter_variable> in range(len(iterable)):
+    print(f"index is {counter_variable}")
+    print(f"element at index {counter_variable} is {iterable[counter_variable]}")
+'''
+
+
+# Exercise 1:
+#name = "María"
+                # 5 letters     
+#for i in range(len(name)):
+                        # index     access the character at index
+#    print(f"element at index {i} is {name[i]}")
+'''             Output:
+element at index 0 is M
+element at index 1 is a
+element at index 2 is r
+'''
+
+        # 1.2
+#listOfNums = [4 , 16, 33, 39]
+#for num in listOfNums:
+#    print(num)
+
+
+# Exercise 2:
+'''         Definition:
+    while loops -> is a control flow  statement that enables you to
+    runs code repeatedly WHILE a specified condition is True. 
+'''
+
