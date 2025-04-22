ba3553a49b2bbd4b456e111752f962c015dd0e23
Maria Delacruz <mar23iaa@gmail.com>
2025-04-22 09:53:58 -0700

    added class notes

---
+++ class/April22.py
@@ -0,0 +1,24 @@
+                                                    # Data Structures
+'''          List
+Mutable, ordered, non-unique items (can have mixed data types)
+'''
+
+            # Exercise 1:       
+# Empty List
+# veggies = []
+
+# List of fruits
+#fruits = ["papaya", "pomegranate", "kiwi"]
+# first element
+#print(fruits[0])
+# last value
+#print(fruits[-1])
+
+#replace papaya with orange
+#fruits[0] = "orange"
+#print(fruits)
+
+            # Exercise 2:
+# Set -> unique items, unordered, { }, mutable
+colors = {"green", "orange", "purple"}
+print(colors)
