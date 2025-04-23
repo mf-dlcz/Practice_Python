757f7ad6f250fe8000d868cb1cae78d3109e0c86
Maria Delacruz <mar23iaa@gmail.com>
2025-04-22 21:32:46 -0700

    Organized my notes from today's lesson

--- class/April22.py
+++ class/April22.py
@@ -8,34 +8,43 @@ Mutable, ordered, non-unique items (can have mixed data types)
 # veggies = []
 
 # List of fruits
-#fruits = ["papaya", "pomegranate", "kiwi"]
+# fruits = ["papaya", "pomegranate", "kiwi"]
 # first element
-#print(fruits[0])
+# print(fruits[0])
 # last value
-#print(fruits[-1])
+# print(fruits[-1])
 
-#replace papaya with orange
-#fruits[0] = "orange"
-#print(fruits)
+# replace papaya with orange
+# fruits[0] = "orange"
+# print(fruits)
+
 
             # Exercise 2:
-# Set -> unique items, unordered, { }, mutable
-# box of markers
-#colors = {"green", "orange", "purple"}
-#print(colors)
+'''         Set 
+- unique items, unordered, { }, mutable
+- reminds me of: a box of markers, since each color is unique in a box. 
+'''
+# colors = {"green", "orange", "purple"}
+# print(colors)
 
 
             # Exercise 3:
-# Tuple Data Structure -> Sequenced (order, index), immutable elements
-# Curb side pick up
-#zoo = ("Python", "Giraffe", "Koala")
-#print(zoo)
+'''         Tuple 
+- Sequenced (order, index), immutable elements
+- reminds me of: Curb side pick up. Once order is placed can't be modified.
+'''
+
+# zoo = ("Python", "Giraffe", "Koala")
+# print(zoo)
 # access individual elements using their index
-#print(zoo[2]) # Koala
+# print(zoo[2]) # Koala
 
 
             # Exercise 4:
-# Dictionary ->  Mutable, unordered key-value pairs (NO INDEX)
+'''         Dictionary
+- Mutable, unordered key-value pairs (NO INDEX)
+'''
+
 person = {"name": "Tina", "age": 97, "city": "Why does it matter"}
-#must include key to access value
+# must include key to access value
 print(person["city"])
