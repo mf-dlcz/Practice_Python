dabf35c257fa17206d7ff43aafaddd4f29d4002b
Maria Delacruz <mar23iaa@gmail.com>
2025-04-23 21:29:14 -0700

    Added notes from today's lesson

---
+++ class/April23.py
@@ -0,0 +1,20 @@
+'''
+unpacking
+concatenating collections
+iterating over collections
+'''
+
+# Unpacking
+# This is my tuple()
+# coordinates = (23, 45, 18)
+# # Extracts & assigns a variable to each coordinate
+# x, y, z = coordinates
+
+# print(f'x: {x}, y: {y}, z: {z}')
+
+# x = x + 24  # 47
+# coordinates = x, y, z
+# print(coordinates)
+
+#24:09
+#color_intensity = 
