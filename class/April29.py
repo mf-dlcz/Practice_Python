70646df4dab11e63cfd2a6b34c11c62478c4fdbe
Maria Delacruz <mar23iaa@gmail.com>
2025-04-29 09:50:35 -0700

    Worked on some exercises

---
+++ class/April29.py
@@ -0,0 +1,23 @@
+# Functions & Methods:
+
+nums = [65, 71, 23, 88]
+# print(len(nums))
+# print(sum(nums))
+# print(min(nums), max(nums))
+
+# .append adds element to the end
+# nums.append(100)
+# print(nums)
+
+# .insert takes (index, number being added) 
+# nums.insert(2, 30)
+# print(nums)
+
+# .clear() deletes all the elements in the list
+# nums.clear()
+# print(nums)
+
+# .copy() creates a new copy and puts that copy in new_nums
+# new_nums = nums.copy()
+# print(new_nums, nums)
+
---
+++ myPracticeExercises/April28_2.py
@@ -0,0 +1,13 @@
+''' Develop a flashcard type game where:
+Based on the number a user enters, user will get a question 
+User has 3 attempts to guess the answer to that question.'''
+
+#create a ditionary with fun questions as keys and answers as values
+q_and_a = {
+    "What is the capital of France?": "Paris",
+    "Which planet is known as the Red Planet?": "Mars",
+    "What is the largest mammal?": "Blue Whale",
+    "What is the name of Harry Potter's owl?":"Hedwig",
+    "How many sides does a hexagon have?" : "6",
+    "In which country was the game of chess invented?" : "India"
+}
