11297fe352dcf1932de6d9b6928e6809d96b61c5
Maria Delacruz <mar23iaa@gmail.com>
2025-05-01 10:05:08 -0700

    Added exercises from today's lesson

---
+++ class/May1.py
@@ -0,0 +1,29 @@
+countries = {
+    "USA": ["New York", "Los Angeles", "Chicago"],
+    "UK": ["London", "Manchester", "Birmingham"],
+    "France": ["Paris", "Nice"]
+}
+
+
+# What would countries[0] print?            #name[key]
+# print(countries[0])         #Error
+
+
+# What would countries["USA"][0] print?
+# print(countries["USA"][0])      #New York
+
+
+# What would countries["UK"][-1] print? 
+# print(countries["UK"][-1])
+
+
+# What would countries["France"][4] print?
+# print(countries["France"][4])       #List index out of range
+# print(countries.get(["France"][4]))
+
+
+# How do you add a new city "Boston" to USA's list?
+
+
+
+# How do you add a new country "Germany" with cities ["Berlin", "Hamburg", "Munich"]?
