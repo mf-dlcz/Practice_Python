75e7e9b08600a022462938bd3bfb6d1460cd85f3
Maria Delacruz <mar23iaa@gmail.com>
2025-05-04 23:47:13 -0700

    Worked on a couple exercises

---
+++ myPracticeExercises/May4.py
@@ -0,0 +1,26 @@
+# Practice Problems:
+
+# Accessing the items of a dictionary
+
+thisDictionary = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1999
+}
+
+'''         Getting the value of brand
+'''
+# print(thisDictionary["brand"])
+# print(thisDictionary["year"])
+
+
+# Using the .get() method
+# print(thisDictionary.get("model"))
+# print(thisDictionary.get("name"))   #Got NONE because it doesn't exist
+# print(thisDictionary["name"])       #Got Error because it doesn't exist
+
+
+'''         Getting the keys of dictionary
+
+'''
+# print(thisDictionary.keys())
