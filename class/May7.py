3d8d7033fb25c70f3ab3ff53277bb33e94168682
Maria Delacruz <mar23iaa@gmail.com>
2025-05-07 10:34:50 -0700

    Added a new folder and file for chapter notes and added the rest of the exercises from today's lesson

--- class/May7.py
+++ class/May7.py
@@ -1,4 +1,4 @@
-# Function Definitions:
+#                                   Function Definitions:
 
 def feed_animals():
     food = "bananas"
@@ -19,3 +19,36 @@ rename_zoo()
 print("The zoo has been renamed to: ", zoo_name)
 
 print("End of zoo exercise!")
+
+
+#                                     SCOOP EXAMPLE:
+def my_function():
+    global x
+    x = 20
+    print("Inside the function, x has a value: ", x)
+
+x = 10
+
+my_function()
+print(x)
+
+
+#                                     Superhero Academy: Parameters and arguments
+
+#parameters - placeholders in the function definition
+def greeting(hero_name, power):
+    print(f"Hi!, I am {hero_name} and my power is {power}!")
+    
+#send arguments in the function call
+#hero = input("Enter a name for the hero: ")
+#greeting("whiteboarding", hero)   #positional arguments - order matters 
+
+#keyword arguments - order does not matter!
+# greeting(power = "experimenting", hero_name = "Dr. Data")
+
+#default values for parameters
+def equip_hero(name='Captain Python', costume = 'cape', mask = True):
+    print(f"{name} is wearing a {costume}, and has a mask - {mask}")
+    
+equip_hero()
+equip_hero("CodeMan", "No cape")
