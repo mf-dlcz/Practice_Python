c26895dfecd15da1a07d95ec50b9f81a502515ad
Maria Delacruz <mar23iaa@gmail.com>
2025-05-07 20:16:15 -0700

    Added exercises from a previous day

---
+++ myPracticeExercises/May3.py
@@ -0,0 +1,36 @@
+# A Few Exercises:
+
+# min_y = 15
+# y = 20 - 5
+
+# if min_y >= y:
+#     print('y is at or above the minimum')
+# elif min_y == y:
+#     print('y is at the minimum!')
+# elif min_y <= y:
+#     print('y is at or less than the minimum!')
+# else:
+#     print('y is unknown')
+
+
+# Code ran 3 times              3, 5, 7
+# for i in range(3, 8, 2):
+#     print(i)
+
+
+# Output is 
+# y = (8 + 2 * 2) / 3 * (10 - 6)
+# 10 * 2     20/  
+# print(y)
+
+
+# Value of count
+# count = 10
+# count += 11
+
+# print(count) 21
+
+
+# What will this print out?
+# mystring = "Cloud computing is Fun"
+# print(mystring[7:11])       # Output 7 - 10      -> ompu
