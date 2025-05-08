3b87252abe4eab584a6ca66b462ab81df8de0830
Maria Delacruz <mar23iaa@gmail.com>
2025-05-07 19:22:14 -0700

    Added notes for chapter 1 & chapter 2

---
+++ chapNotes/chap1_2.py
@@ -0,0 +1,146 @@
+#                                                               Chapter 1 & 2 Summary:
+
+#                           Syntax and Structure in Python:
+"""
+When you write code in a programming language, proper syntax allows 
+your programs to be interpreted and translated into machine code, so 
+that your computer can produce the output you want. There are some 
+important guidelines to understand as you start exploring language-specific 
+instruction. Expand each of the following five categories to learn more.
+"""
+
+
+#                           Whitespace is important:
+"""
+Whitespace is the space between text. Python has an uncluttered look 
+because it uses whitespace to define the end of a line of code, or 
+to group blocks of code together.
+"""
+
+
+#                           A grouping symbol always has a partner:
+"""
+There are a few symbols that signify a special type or special group of 
+data. These come in opening and closing pairs. Leaving out one member 
+of the pair will cause an error and your code will not run.
+"""
+
+
+#                          Comments help everybody:
+"""
+You can use special notation to write notes and explanations into your code. 
+Remember, programming happens on teams. It's important that the code you write 
+can be understood, adjusted, or used by someone else. That someone could even 
+be future you.
+"""
+
+
+#                          Python is case-sensitive:
+"""
+This means that capital A is not the same as lowercase a. Any difference in 
+case from word to word is enough to ensure that they are evaluated as 
+two different things.  
+"""
+
+
+#                           Be Aware of keywords:
+"""
+There is a set of reserved words in Python that are used only in specific 
+circumstances. You will learn what they are (and what they do) as you go. 
+Don't worry about trying to memorize them now. Understanding that keywords 
+exist can be helpful as you're getting started.
+"""
+
+
+#                           Variables:
+"""
+A variable is a container for storing data values. You use variables 
+to store information that Python can reference and use. Instead of using 
+a stored value directly, you can use a variable instead. Variables could 
+be any number of things, such as a string of text, a list, or an object. 
+Using variables makes your code flexible and reusable.
+"""
+
+
+#                           Operators:
+"""
+Operators are used to perform operations on variables and values. 
+Examples of operators include arithmetic operators, assignment 
+operators, and comparison operators.
+"""
+
+
+#                           Python's built-in data types:
+"""
+Python can store many types of data. For example, a person's name 
+is stored as a text value and their age is stored as a numeric value. 
+Data types help you prevent mistakes, like attempting to add a string 
+"Alice" to a number 35. Examples of data types include text and numeric. 
+You can use casting to convert a variable’s data type into another 
+data type.
+"""
+
+
+#                           Working with String
+#                           Indexing & Slicing:
+"""
+Strings are organized into entities called elements. In a string, each 
+character is an element. Python stores elements in a sequence. You can 
+manipulate strings at the element level by using indexing and slicing. 
+Indexing is a method to access individual elements in a sequence. To 
+reference a specific element, you use its index number in square 
+brackets next to the sequence name. This is commonly known as slicing.
+"""
+
+
+#                           Modifying Strings:
+"""
+Python provides several string methods to perform operations on strings.
+            upper() and lower() convert characters to lowercase and uppercase
+            strip() removes extraneous space
+            replace() replaces a specified string with a substring
+You can also modify strings using the following:
+Concatenation: Combine two or more strings into a single string
+Escape characters: Represent special characters that are illegal in a string
+"""
+
+
+#                           User Input:
+"""
+The input() function allows you to pause your program and prompt a 
+user to enter text. Once Python receives that information, it assigns 
+it to a variable. The input() function interprets the information that 
+the user enters as a string.
+"""
+
+
+#                           Conditionals:
+"""
+An important part of programming involves reviewing a set of conditions, 
+and determining which action to take based on them. You use these conditions 
+most commonly in the context of if statements and loops.
+
+If statements allow you to examine conditions and respond appropriately. 
+They consist of one condition and one action.
+
+If you need to test multiple conditions with different actions, you 
+can include additional code blocks to an if statement. Use the elif 
+keyword (short for "else if") to check additional conditions after the first if statement.
+
+Use the else keyword to address anything that does not match the preceding conditions.
+"""
+
+
+#                           Loop Statements:
+"""
+Loops are instructions that repeat multiple times as long as a 
+condition is met. Python has for loops and while loops.
+
+            For loops iterate over a sequence of items, 
+            such as a list or a string, and perform an 
+            action on each element in the sequence.
+
+            A while loop is a control flow statement that 
+            allows you to execute code repeatedly while 
+            a specified condition is true.
+"""
