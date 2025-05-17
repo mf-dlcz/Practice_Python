f1ada2f158116a0e6ff0ad9edeeb6793a42a7752
Maria Delacruz <mar23iaa@gmail.com>
2025-05-16 22:59:25 -0700

    Started working with sets

---
+++ myPracticeExercises/workingWithSets.py
@@ -0,0 +1,18 @@
+#   #################       Working with Sets:
+
+"""
+Sets have unique values. 
+Use a set constructor to convert
+a list to a set which will remove the
+duplicate values.
+"""
+
+list = [1, 1, 2, 2, 3, 4, 2, 1, 5, 6, 7, 4, 3, 4, 5, 6]
+
+# convert to set to remove duplicate values
+my_set = set(list)
+
+# print(my_set)
+
+
+#      ###################        SET Methods:
