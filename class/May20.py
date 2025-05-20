d00a65202ab9555100da46985d0e2ec85473e328
Maria Delacruz <mar23iaa@gmail.com>
2025-05-20 10:45:07 -0700

    Added another example

--- class/May20.py
+++ class/May20.py
@@ -51,3 +51,63 @@ car1 = Car("Black", "Lexus", 2020)
 car1.move_forward(1140)
 car1.fill_gas()
 print(car1.read_miles())
+
+
+
+
+
+
+
+
+
+
+
+# Parent Class
+class Shape:
+
+    def __init__(self, color):
+        self.color = color
+
+    
+    def calc_area(self):
+        pass
+
+    def get_color(self):
+        print(self.color)
+
+# inheritance class
+class Rectangle(Shape):
+
+    def __init__(self, color, width, height):
+        # call parents init
+        # with super, we don't need self
+        super().__init__(color)
+        self.width = width
+        self.height = height
+
+    # overrides the behavior of the parent class
+    def cal_area(self):
+        return self.width * self.height
+
+
+class Circle(Shape):
+    
+    def __init__(self, color, radius):
+        super().__init__(color)
+        self.radius = radius
+
+    def cal_area(self):
+        return 3.14 * self.radius * self.radius
+
+# create a rectangle object
+rec1 = Rectangle("Turquoise", 5, 2)
+rec1.get_color()
+area = rec1.cal_area()
+print(f"The area of the rectabgle is {area}")
+
+
+# creates a circle object
+circle1 = Circle("Grey", 4)
+circle1.get_color()
+circle_area = circle1.calc_area()
+print("The area of the circle is ", circle_area)
