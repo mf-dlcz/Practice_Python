b7a9641c38ec9b5506979a3e7aaf7dcfc3ee5506
Maria Delacruz <mar23iaa@gmail.com>
2025-05-23 23:34:39 -0700

    Began the chapter on Functions. Yay

---
+++ myPracticeExercises/functions.py
@@ -0,0 +1,35 @@
+"""
+FUNCTIONS ->
+
+- Functions are blocks of code that perform a specific Task.
+- Break your code into reusable, smaller pieces and they make
+your code easier to read & maintain. 
+- If you need to perform a task multiple times, you can use
+functions to avoid repeating the same code.
+"""
+
+#       Example #1: 
+def my_function():
+    print("I miss you")
+
+# my_function()
+
+
+#       #################################               MATH FUNCTIONS:
+"""
+-   Functions that are predefined don't need the def
+    keyword.
+-   Math functions are pre-build functions you can use to
+    perform mathematical tasks on numbers.
+-   Use the min() and max() functions to find the lowest and
+    highest values in a list of numbers.
+"""
+
+
+numbers = [60, 20, 11, 4, 12, 10]
+min_val = min(numbers)
+max_val = max(numbers)
+
+print('This is the minimum values: ',min_val)
+print('This is the maximum value: ', max_val)
+
