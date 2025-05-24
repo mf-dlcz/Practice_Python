9031007b57b453fc67d74f97efc7954e6d109178
Maria Delacruz <mar23iaa@gmail.com>
2025-05-23 17:43:30 -0700

    Wrote notes

---
+++ chapNotes/chap3_4.py
@@ -0,0 +1,20 @@
+"""
+DATA STRUCTURES & ITERABLES:
+"""
+
+#       Data Structures:
+"""
+Data structures are the fundamental building blocks of any programming language, 
+including Python. They make possible efficient storage, processing, and retrieval 
+of data. In this topic, you explored four of Python's built-in data structures. 
+Previously, you learned about primitive data structures like integers, floats, and strings.
+"""
+
+#       ITERABLES:
+"""
+An iterable is an object that can be looped through using a for loop. Built-in iterables
+for Python include lists, sets, tuples, and dictionaries. This is useful for when you 
+want to perform the same action, or set of actions, on each item in the collection.
+"""
+
+#       CHARACTERISTICS OF LISTS, SETS, DICTIONARIES & TUPLES:
