b6d4693b55c52b3d945cfc236005bf4f6055ec83
Maria Delacruz <mar23iaa@gmail.com>
2025-05-26 22:57:03 -0700

    Began a new section on OOP

---
+++ myPracticeExercises/classes-and-objects.py
@@ -0,0 +1,53 @@
+import json
+
+'''                     CLASSES & OBJECTS:
+
+-   An attributes describe something about a class or object.
+-   Methods define what a class or object can do.
+-   Object method and attributes are specific to a particular instance of a class.
+-   Class methods and attributes are shared by all instances of a class.
+
+'''
+
+
+
+#               CLASSES:
+'''
+-   A class is like a blueprint for creating new objects.
+-   You can define, at the class level, what data the
+    new object should carry and the functions it can perform.
+'''
+
+
+
+#               OBJECTS:
+'''
+-   An object is an instance of a class.
+-   It is a collection of data that describes the object (attributes)
+    and define what it can do(methods).
+'''
+
+
+
+#               Attribues:
+'''
+-   Attributes describe an object's state.
+-   This can be something descriptive like color or size.
+-   Or, it could reflect of state of being, like 'on' or 'off'
+'''
+
+
+
+#               METHODS:
+'''
+-   Methods describe what an object can do.
+-   These are the functions you can build into an object.
+'''
+
+
+
+#       ############################################################################                EXPLORING OBJECTS & NOTATION:
+'''
+    1.  What are objects?
+        
+'''
