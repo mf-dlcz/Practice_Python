771501a191dcf6af1f712c3318c356de492c841e
Maria Delacruz <mar23iaa@gmail.com>
2025-06-01 14:27:09 -0700

    Added code to test my classes

--- myPracticeExercises/Chapter_6_7/employee_network.py
+++ myPracticeExercises/Chapter_6_7/employee_network.py
@@ -5,7 +5,6 @@ class Employee:
     # class attribute to track current number of employees
     employee_count = 0
     
-    
     def __init__(self, name, email, hire_date):
         """
         Constructor method for Employee class
@@ -18,6 +17,13 @@ class Employee:
         self.hire_date = hire_date
         Employee.employee_count += 1
         
+        # initates an empty list to hold all posts
+        self.posts = []
+
+    def post_message(self, message):
+        post = Post(self, message)
+        self.posts.append(post)
+        return post
 
 class Post:
 
@@ -31,3 +37,25 @@ class Post:
     
     def edit_post(self, new_comment):
         self.message = new_comment
+
+
+"""
+Testing my classes
+"""
+# create a new employee
+e1 = Employee("Mary Major", "mary.major@example.com", "07/12/2021")
+
+# Use the post message method to create new Post objects
+e1.post_message("hello")
+
+# Capture the function return in a variable so you can reference the Post
+second_post = e1.post_message("second message")
+
+# Use the edit message method from the Post class to edit the message
+second_post.edit_post("edited message")
+
+# Check post to see that it includes the Employee as author
+print (second_post.author.name)
+
+# Use a list comprehension to print each message on the Employee's posts list. 
+print ([post.message for post in e1.posts])
