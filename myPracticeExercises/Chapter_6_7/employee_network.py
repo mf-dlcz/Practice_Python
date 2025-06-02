b7a3bc170aa6512bfc1110ee573d0fe5d7dc92f4
Maria Delacruz <mar23iaa@gmail.com>
2025-06-02 08:12:19 -0700

    Added a like class and function:

--- myPracticeExercises/Chapter_6_7/employee_network.py
+++ myPracticeExercises/Chapter_6_7/employee_network.py
@@ -16,72 +16,86 @@ class Employee:
         self.email = email
         self.hire_date = hire_date
         Employee.employee_count += 1
-        
         self.total_score = 0             # initializes a total_score variable to track points
         self.posts = []                  # initializes an empty list to hold all posts
         self.comments = []               # initializes an empty list to hold all comments
-
-    # Creates a new post
-    def post_message(self, message):
+    def post_message(self, message):     # creates a new post
         post = Post(self, message)
         self.total_score += 5
         self.posts.append(post)
         return post
-
-    # creates a new comment in a post
-    def comment_on_post(self, message, post):
+    def comment_on_post(self, message, post, points = 1):           # creates a new comment in a post
         comment = Comment(self, message, post)
+        self.total_score += points
         post.comments.append(comment)               # comment is added to the comments list
         self.comments.append(comment)               # appends comment to the employee object
+        return comment                              # returns comment object
+    def like_post(self, post, points = 1):          # creates a new comment on a post.
+        like = Like(self, post)                     # creates a new Like object with an employee as author & the orig post
+        self.total_score += points
+        post.likes.append(self)
 
                                         # Subclasses:
 class Engineer(Employee):
     def __init__(self, name, email, hire_date):
         super().__init__(name, email, hire_date)
-        self.department = 'engineering'
-
+        self.department = 'Engineering'
     def comment_on_post(self, message, post):           # overrides the parent comment_on_post method
-        if post.author.department is 'engineering':
+        if post.author.department == 'Engineering':
             points = 1
         else:
             points = 5
-        comment = Comment(self, message, post)
-        self.total_score += points
-        post.comments.append(comment)                   # appends comment to the post object
-        self.comments.append(comment)                   # appends comment to the Employee Object
-        return comment
+        return super().comment_on_post(message, post, points)
 
 class Marketer(Employee):
     def __init__(self, name, email, hire_date):
         super().__init__(name, email, hire_date)
-        self.department = 'marketing'
+        self.department = 'Marketing'
+    def comment_on_post(self, message, post):
+        if post.author.department == 'Marketing':
+            points = 1
+        else:
+            points = 5
+        return super().comment_on_post(message, post, points)
+
+class UX(Employee):
+    def __init__(self, name, email, hire_date):
+        super().__init__(name, email, hire_date)
+        self.department = 'UX Designer'
+    def comment_on_post(self, message, post):
+        if post.author.department == 'UX Designer':
+            points = 1
+        else:
+            points = 5
+        return super().comment_on_post(message, post, points)
 
                                         # Post Class
 class Post:
-
-    #constructor
+    # constructor
     def __init__(self, message, author):
         self.message = message
         self.author = author
-
-        # initiates a list that holds comments on the post
-        self.comments = []
-    
+        self.comments = []                         # initiates a list that holds comments on the post
     def edit_post(self, new_comment):
         self.message = new_comment
 
                                         # Comment class
 class Comment:
-
-    # Constructor
+    # constructor
     def __init__(self, author, message, post):
         self.author = author
         self.message = message
         self.post = post
-
     def edit_message(self, new_message):
         self.message = new_message
 
+class Like:
+    # constructor
+    def __init__(self, author, post):
+        self.author = author
+        self.post = post
+    def unlike(self, post):
+        self.post.likes.remove(self)
 
 
 """
@@ -91,10 +105,12 @@ class Comment:
 e1 = Employee("Mary Major", "mary.major@example.com", "07/12/2021")
 e2 = Employee("Pat Candella", "pat.candella@example.com", "12/1/2022")
 engineer1 = Engineer('Michael Mike', 'michael.mike@example.com', '06/1/2023')
-marketer1 = Marketer('Arisa Blank', 'arisa.blank@example.com', '08/15/22')
+marketer1 = Marketer('Arisa Blank', 'arisa.blank@example.com', '08/15/2022')
+ux1 = UX('Nikki Nike', 'nikki.nike@example.com', '10/10/2023')
 
 print(f'Name: {engineer1.name} Email: {engineer1.email} Hire Date: {engineer1.hire_date}')
 print(f'Name: {marketer1.name} Email: {marketer1.email} Hire Date: {marketer1.hire_date}')
+print(f'Name: {ux1.name} Email: {ux1.email} Hire Date {ux1.hire_date}')
 
 # print(f'EMPLOYEE COUNT: {Employee.employee_count}')
 
@@ -102,15 +118,16 @@ print(f'Name: {marketer1.name} Email: {marketer1.email} Hire Date: {marketer1.hi
 intro_message = e2.post_message("Hi all! So excited to be joining the company!")
 engineer1_message = engineer1.post_message(f"Hi, My name is {engineer1.name} I'm super excited to be here!")
 marketer1_message = marketer1.post_message(f"Hi, I'm {marketer1.name} It's a pleasure to meet you all!")
+ux1_message = ux1.post_message(f"Hi, this is {ux1.name}, it's good to meet everyone!")
 
-print(engineer1.total_score)
-print(marketer1.total_score)
-
+print(f'Engineer total score: {engineer1.total_score}')
+print(f'Marketer total score: {marketer1.total_score}')
+print(f'UX Designer total score: {ux1.total_score}')
 
 # Use the returned post to comment on the message
 e1.comment_on_post("Welcome to the team!", intro_message)
 e2.comment_on_post("Thanks!", intro_message)
-
+ux1.comment_on_post("Great!", intro_message)
 
 # Another example of a new post with comment exchange
 workshop_post = e1.post_message("I just attended a workshop.")
