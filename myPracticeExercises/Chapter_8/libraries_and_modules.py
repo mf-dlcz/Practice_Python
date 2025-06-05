c94d07059d3ca297baaf4b498b709fb1e3066d8c
Maria Delacruz <mar23iaa@gmail.com>
2025-06-04 23:34:43 -0700

    Started a new chapter on modules and libraries

---
+++ myPracticeExercises/Chapter_8/libraries_and_modules.py
@@ -0,0 +1,42 @@
+'''
+                        Libraries & Modules:
+'''
+
+
+'''             Module:
+A module is a file that contains a collection of related functions and 
+classes ready for use in Python programs so that developers can avoid 
+writing code from scratch.
+'''
+
+
+'''             Libraries:
+A library is a collection of related modules. Whenever you need to use a 
+module, you import it from its library
+'''
+
+
+#       #######################################################                 WORKING WITH MODULES:
+
+'''             Scripts:
+Scripts are files that are meant to be run directly. When you copy and paste 
+code into your example.py file, you run it and get some output. That is a script. 
+In the world of professional programming, you will work with scripts that won't 
+necessarily print anything, but are meant to do something. They might apply a 
+series of functions to some data, create new objects, or send data to a new file.
+'''
+
+
+'''             Modules:
+Modules are files that are intended to be imported into running scripts. They contain 
+definitions and statements (like functions, classes, and variables) that a script can 
+use. Running a module does not generally produce output. 
+
+Think of the employee networking program you created while studying classes and objects. If you 
+didn't include any test code at the end of that file, it would not produce output on its own 
+and can be considered a module.
+
+There are many modules included as part of Python's standard library. Modules and libraries 
+are also available for install. Additionally, you can write your own modules for use in 
+your scripts.
+'''
