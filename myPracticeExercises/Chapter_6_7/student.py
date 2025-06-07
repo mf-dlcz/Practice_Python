346665b9050d90b05386ef9aecf9f5caade71c05
Maria Delacruz <mar23iaa@gmail.com>
2025-06-06 22:54:25 -0700

    Added a new file to practice OOP

---
+++ myPracticeExercises/Chapter_6_7/student.py
@@ -0,0 +1,11 @@
+#       Student
+
+class Student:
+    name = str
+
+    def __init__(self, color = str, quarter = int, hobby = str):
+        self.color = color
+        self.quarter = quarter
+        self.hobby = hobby
+
+    
