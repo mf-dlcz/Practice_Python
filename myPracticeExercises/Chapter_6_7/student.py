52334ff55e770144f294f386f0ac68a28c37507c
Maria Delacruz <mar23iaa@gmail.com>
2025-06-07 17:32:09 -0700

    Continues working on my student class

--- myPracticeExercises/Chapter_6_7/student.py
+++ myPracticeExercises/Chapter_6_7/student.py
@@ -1,11 +1,23 @@
 #       Student
 
 class Student:
-    name = str
-
-    def __init__(self, color = str, quarter = int, hobby = str):
+    def __init__(self, name: str, color: str, quarter: int, hobby: str):
+        self.name = name
         self.color = color
         self.quarter = quarter
         self.hobby = hobby
 
-    
+    def print_hobby(self):
+        self.hobby
+        return f'{self.name} loves {self.hobby}'
+
+    def print_all_details(self):
+        self.name
+        self.color
+        self.quarter
+        self.hobby
+        return self.name, self.color, self.quarter, self.hobby
+
+
+student1 = Student('Maria', 'grey', 1, 'hiking')
+print(student1.print_hobby())
