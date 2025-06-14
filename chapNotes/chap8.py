45b1f52731b2233ce75b434cbefb0fe41ee79bd3
Maria Delacruz <mar23iaa@gmail.com>
2025-06-13 23:26:02 -0700

    Added chapter 8 notes

---
+++ chapNotes/chap8.py
@@ -0,0 +1,39 @@
+#           Libraries & Modules:
+'''
+Libraries and modules offer common tools for working with different types 
+of data and a structure that helps to keep code organized and practical to 
+maintain.
+'''
+
+#           Import Modules:
+'''
+When you import a module into your script, you can make the variables, functions, 
+and classes defined in the module available from within your script.
+'''
+
+#           math & datetime module:
+'''
+Math and datetime modules are part of Python's standard library. The math 
+module provides a collection of mathematical functions and constants that you 
+can use to process numerical data. The datetime module provide classes for 
+creating datetime objects that can help to capture and format dates and find 
+the difference between points in time.
+'''
+
+#           creating modules:
+'''
+You can create your own modules designed to work with your specific data. 
+That means you can write custom functions, classes, and other objects that 
+meet your specific needs. Because modules are Python files, they're 
+convenient to share. They help to keep your code modular so that you can
+make small fixes to individual components of the code without affecting 
+the larger code base.
+'''
+
+#           importing packages:
+'''
+The Python package installer, pip, is included automatically with most Python 
+distributions. That means you can use it to install and update third-party 
+packages with a single line of code. Many third-party packages include specialized 
+tools for working with specific systems or types of data.
+'''
