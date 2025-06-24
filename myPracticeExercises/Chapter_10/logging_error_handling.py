e90a1a4cb2570566572f90e32f60f834130c872d
Maria Delacruz <mar23iaa@gmail.com>
2025-06-23 23:28:08 -0700

    Added additonal notes on error handling, and logging levels

--- myPracticeExercises/Chapter_10/logging_error_handling.py
+++ myPracticeExercises/Chapter_10/logging_error_handling.py
@@ -1,6 +1,7 @@
 #   Logging:
 
 '''
+                            What is logging?
 -   Logging refers to the practice of recording events and data about an application's behavior as the application code runs.
 -   They are meant to record events and information in a way that can be reviewed later.
 -   You can create multiple logging objects to report about different parts of your code.
@@ -15,5 +16,40 @@ Monitoring & Performance Analysis:  Logging provides monitoring and performance 
                                     behavior over time. Developers can include logs that measure efficiency of running code. They 
                                     can use those logs to detect weak performance and resolve inefficiencies.
 
+Security & Compliance:  Logs can be used to identify and fix security vulnerabilities in application code, as well as ensure that 
+                        security policies are being followed. Regular reviews of audit logs can help organizations identify and 
+                        improve weaknesses in their systems.
+'''
+
 
 '''
+                            Error Handling:
+
+-   refers to the process of anticipating, detecting, responding to and recovering from errors that occur when an application runs.
+-   When you anticipate where errors might occur, error handling provides a way to resolve or work around the problems without stopping 
+    your program or losing data. Without proper error handling, software can fail in unpredictable ways, leading to data loss and 
+    security vulnerabilities.
+'''
+
+
+'''
+                            Logging Levels:
+
+Level:          When It's Used:                                                                                     Least Severe:
+Debug           Detailed information, typically of interest only when diagnosing problems.          
+
+Info            Confirmation that things are working as expected.
+
+Warning         An indication that something unexpected happened, or indicative of some 
+                problem in the near future (for example, ‘disk space low’). The software 
+                is still working as expected.
+
+Error           Due to a more serious problem, the software has not been able to perform some function.
+
+Critical        A serious error, indicating that the program itself may be unable to continue running.              Most Severe:
+
+DOCUMENTATION:
+https://docs.python.org/3/howto/logging.html#when-to-use-logging
+
+'''
+
