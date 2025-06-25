7ffa907a1edf803c213f3829ee5eddf16f251b58
Maria Delacruz <mar23iaa@gmail.com>
2025-06-24 23:26:17 -0700

    Working on a logging exercise

---
+++ myPracticeExercises/Chapter_10/output.py
@@ -0,0 +1,14 @@
+import logging
+
+# The only messages that were logged are WARNING,ERROR & CRITICAL
+# Only the messages of warning level and higher are logged
+'''
+logging.debug('This is a debug message')
+logging.info('This is an info message')
+logging.warning('This is a warning message')
+logging.error('This is an error message')
+logging.critical('This is a critical message')
+'''
+
+
+#                   EXERCISE: Logging Practice
