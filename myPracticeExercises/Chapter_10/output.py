d779ea44ff5d7746aca4e89ddc6a206bd21606cb
Maria Delacruz <mar23iaa@gmail.com>
2025-06-24 23:35:54 -0700

    Added a new file

--- myPracticeExercises/Chapter_10/output.py
+++ myPracticeExercises/Chapter_10/output.py
@@ -24,3 +24,31 @@ logging.warning('This is a warning message')
 logging.error('This is an error message')
 logging.critical('This is a critical message')
 '''
+
+############################                    EXERCISE:
+
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
+def add_numbers(x, y):
+    """Adds two numbers and returns the sum"""
+    if type(x) != type(y):
+        logging.warning("Args may not be compatible")
+    
+    if type(x)==str and type(y)==str:
+        logging.error("Result is concatenated string")
+    
+    if (type(x)!=type(y)) and (type(x) in (bool,str) or type(y) in (bool, str)):
+        logging.critical("Result '' cannot be processed")
+        return ""
+        
+    logging.debug("x = {}, y = {}".format(x,y))
+    logging.info("Function returns {}".format(x+y))
+    return x+y
+    
+def main():
+    add_numbers(2, 3)
+    
+if __name__ == "__main__":
+    main()
