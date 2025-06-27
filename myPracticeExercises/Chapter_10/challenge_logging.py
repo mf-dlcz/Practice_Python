ac592af263ab366dc1d34c0b1331015c52f7c161
Maria Delacruz <mar23iaa@gmail.com>
2025-06-26 23:31:45 -0700

    Began the challenge

---
+++ myPracticeExercises/Chapter_10/challenge_logging.py
@@ -0,0 +1,9 @@
+# Using the logging Module
+
+import logging
+
+'''
+Use the logging module to write log messages to a file named example.log. 
+Each log message should include the time it was logged, the level name, 
+and a message. All logs at level INFO and higher should be logged.
+'''
