e31c81a01c637846581aa6af2a59cc74684a8a54
Maria Delacruz <mar23iaa@gmail.com>
2025-06-28 02:04:33 -0700

    Completed the logging challenge

--- myPracticeExercises/Chapter_10/challenge_logging.py
+++ myPracticeExercises/Chapter_10/challenge_logging.py
@@ -13,8 +13,18 @@ logging.basicConfig(filename = "/Users/mariadelacruz/Documents/AWS Coding Exerci
                         level = logging.INFO)
 
 def process_data():
-    user_input = []
-    user_input = input("Enter a series of numbers, each separated by comma: \n")
-    return user_input
+    logging.info("Inside my fuction")
+    user_input = input("\nEnter a series of numbers, each separated by comma: \n")
+
+    if " " not in user_input:
+        logging.error("User did not enter values separated by a comma")
+        logging.info("Function will return, please try again.")
+        return 'Please try again!'
+    
+    str_list = user_input.split(",")
+    logging.info("User input is %i items long", len(str_list))
+    numb_list = [int(x) for x in str_list]
+    logging.info("Function returns %s", numb_list)
+    return (numb_list)
 
 print(process_data())
