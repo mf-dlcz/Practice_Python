9e8f851d1de795e1adccdc76579fd006be0ad4b5
Maria Delacruz <mar23iaa@gmail.com>
2025-06-30 15:24:37 -0700

    Added additional code to UnitTest

--- class/Chapter__11/unit_test.py
+++ class/Chapter__11/unit_test.py
@@ -18,6 +18,11 @@ class Calculator:
 
     def subtract(self, value):
         self.result -= value
+        
+    def divide(self, value):
+        # if value == 0: 
+        #     raise ZeroDivisionError
+        self.result /= value
 
     def get_result(self):
         return self.result
@@ -25,23 +30,45 @@ class Calculator:
 # test case class
 class TestMathOperations(unittest.TestCase):
     
+    def setUp(self):
+        self.calc = Calculator()
+        self.calc.result = 1138
+    
+    
+    # test the functions ("global")
+    def test_multiply(self):
+        # print('multiply')
+        self.assertEqual(multiply(6, 7), 42)
+    
     def test_add(self):
+        # print('add')
         result = add(2, 3)
         self.assertEqual(result, 5)
         self.assertNotEqual(result, 4)
         
     def test_divide(self):
+        # print('divide')
         result = divide (2, 3)
         # print(result)
         self.assertAlmostEqual(result, .66666, places = 4)
-
-    def test_multiply(self):
-        result = multiply(5, 2)
-        self.assertEqual(result, 10)
-
-    def test_divide(self):
-        result = divide(10, 2)
-        self.assertEqual(result, 5)
+        
+        
+    # Tests the calculator class
+    def test_Calculator_substract(self):
+        print('subtract')
+        
+    def test_Calculator_add(self):
+        # print('add')
+        # print(self.calc.get_result())
+        # self.calc.result = 42
+        # print(self.calc.get_result())
+        self.calc.add(12)
+        self.assertEqual(self.calc.result, 1150)
+        
+    def test_division_zero(self):
+        with self.assertRaises(ZeroDivisionError):
+            self.calc.divide(0)
+        
         
 if __name__ == '__main__':
     unittest.main()
