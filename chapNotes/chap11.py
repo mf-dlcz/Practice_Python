cfbc8d7991ebd4bc739b237c6e3ff7b785746825
Maria Delacruz <mar23iaa@gmail.com>
2025-07-03 21:43:59 -0700

    Added notes to chap 11

---
+++ chapNotes/chap11.py
@@ -0,0 +1,6 @@
+'''
+What are SDKs?
+
+[Software Development Kit], or SDK, is a package of tools that developers can use to create 
+applications for specific platforms, operating systems, or devices.
+'''
